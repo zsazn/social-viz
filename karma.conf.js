a0b0cdda83d93229018451143ba97c26b2c2aca6
Zheng Zhou <zzhou@cerebriai.com>
2020-10-17 12:28:14 -0500

    updated configs - angular, ts, tslint, karma

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/social-viz'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
