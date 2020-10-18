b6bdb69d09dceee0dd8c0f6c2b5aec10392bd332
Zheng Zhou <zzhou@cerebriai.com>
2020-10-18 14:19:35 -0500

    added load data service and some data

--- src/typings.ts
+++ src/typings.ts
@@ -1,10 +1,8 @@
-import { FormControl } from "@angular/forms";
-
 /*
  * File Created: Saturday, 17th October 2020 8:53:23 pm
  * Author: Zheng Zhou (zhengzhou.purdue@gmail.com)
  * -----
- * Last Modified: Sunday, 18th October 2020 1:06:52 am
+ * Last Modified: Sunday, 18th October 2020 2:09:05 pm
  * Modified By: Zheng Zhou (zhengzhou.purdue@gmail.com>)
  * -----
  */
